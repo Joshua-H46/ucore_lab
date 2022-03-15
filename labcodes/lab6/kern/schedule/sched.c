9e8e63ef50536f181be9531fcf2cce43aaa13273
wanghao <Joshua_H@126.com>
2022-03-15 14:29:30 +0800

    Lab6 done

--- labcodes/lab6/kern/schedule/sched.c
+++ labcodes/lab6/kern/schedule/sched.c
@@ -47,7 +47,8 @@ void
 sched_init(void) {
     list_init(&timer_list);
 
-    sched_class = &default_sched_class;
+//    sched_class = &default_sched_class;
+    sched_class = &default_sched_stride_class;
 
     rq = &__rq;
     rq->max_time_slice = MAX_TIME_SLICE;
